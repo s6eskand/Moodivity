5d1c5350124191be91b77a3357ad9a327d984e08
Geordy Decena <geordydecena@MacBook-Pro-3.local>
2020-11-21 15:07:57 -0500

    userlogs complete

--- backend/userprofile/api/urls.py
+++ backend/userprofile/api/urls.py
@@ -1,11 +1,10 @@
 from .views import (
-    UserProfileListCreateView,
-    MoodListCreateView
+    UserProfileListCreateView
 )
 from django.urls import path
 
 urlpatterns = [
     path('', UserProfileListCreateView.as_view()),
+    path('edit/<pk>', UserProfileListCreateView.as_view()),
     path('delete/<pk>', UserProfileListCreateView.as_view()),
-    path('addMood/<pk>', MoodListCreateView.as_view())
 ]
