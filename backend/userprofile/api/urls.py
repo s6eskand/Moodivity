eebdbf4bc97b8e021e416d9d76086249d1d0dcc2
Geordy Decena <geordydecena@MacBook-Pro-3.local>
2020-11-21 05:37:15 -0500

    google api

--- backend/userprofile/api/urls.py
+++ backend/userprofile/api/urls.py
@@ -1,9 +1,11 @@
 from .views import (
-    UserProfileListCreateView
+    UserProfileListCreateView,
+    MoodListCreateView
 )
 from django.urls import path
 
 urlpatterns = [
     path('', UserProfileListCreateView.as_view()),
-    path('<pk>', UserProfileListCreateView.as_view())
+    path('delete/<pk>', UserProfileListCreateView.as_view()),
+    path('addMood/<pk>', MoodListCreateView.as_view())
 ]
