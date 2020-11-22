fe0f2f09527010f2754e964d5834409297e25469
Ryan <sheppardryan@rogers.com>
2020-11-21 22:44:48 -0500

    beginning transcribe()

--- backend/userlogs/api/views.py
+++ backend/userlogs/api/views.py
@@ -4,9 +4,12 @@ from .serializers import UserLogsSerializer
 from ..models import UserLogs
 import argparse
 from google.cloud import language_v1
+from google.cloud import speech
 from django.conf import settings
 from datetime import datetime
 import json
+import base64
+import wave
 
 def analyze(score):
 
@@ -49,6 +52,68 @@ def sentiment(content):
     
     return score
 
+def transcribe(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+
+    wav_file_bytes = base64.b64decode(gcs_uri)
+    # wav_file_bytes = wav_file_bytes.encode("utf-8")
+    # wave_file = wave.open("speech.wav", "wb")
+    # wave_file.setnchannels(1)
+    # wave_file.setsampwidth(2)
+    # wave_file.setframerate(44100)
+    # wave_file.writeframesraw(wav_file_bytes)
+
+    # wave_file_bytes2 = base64.b64decode(wave_file)
+
+    # base64_img_bytes = gcs_uri.encode('utf-8')
+    # print("*************************")
+    # print(len(base64_img_bytes))
+
+    # with open('decoded_image.wav', 'wb') as file_to_save:
+    #     decoded_image_data = base64.decodebytes(base64_img_bytes)
+    #     file_to_save.write(decoded_image_data)
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(
+        uri=wav_file_bytes
+    )
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=44100,
+        language_code="en-US",
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=300)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    num_results = 0
+    average_confidence = 0
+    final_string = ""
+    for result in response.results:
+        best_alternative = result.alternatives[0]
+
+        transcript = best_alternative.transcript
+        final_string += " " + transcript
+
+        confidence = best_alternative.confidence
+        num_results += 1
+        average_confidence += confidence
+
+        result = {
+            "final_string": final_string,
+            "confidence": average_confidence/num_results
+        }
+
+        return result
+
+    print(f"Transcript: {final_string}")
+    print(f"Confidence: {average_confidence/num_results:.0%}")
+
 class UserLogsCreateView(views.APIView):
 
     def get(self, request, pk=None):
@@ -58,7 +123,8 @@ class UserLogsCreateView(views.APIView):
 
     def post(self, request, pk=None):
         dictTemp = request.data
-        mood = sentiment(dictTemp["log"])
+        text = transcribe(dictTemp["audio"])
+        mood = sentiment(text["final_string"])
         dictTemp["mood"] = mood
         analysis = analyze(mood)
         dictTemp["analysis"] = analysis
